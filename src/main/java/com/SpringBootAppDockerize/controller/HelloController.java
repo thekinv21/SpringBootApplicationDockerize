177588c3ba3891ddfe222544ea0d498bb4cd1797
Vadim Kiniabaev <thekinv21@gmail.com>
2026-09-03 17:08:08 +0300

    feat:[master][Vadim]: Hello controller oluşturuldu

--- src/main/java/com/SpringBootAppDockerize/controller/HelloController.java
+++ src/main/java/com/SpringBootAppDockerize/controller/HelloController.java
@@ -1,4 +1,15 @@
 package com.SpringBootAppDockerize.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/api/")
 public class HelloController {
+
+    @GetMapping("/start")
+    public String start() {
+        return "Spring Boot App Dockerize start";
+    }
 }

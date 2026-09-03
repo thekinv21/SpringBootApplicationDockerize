89bd13d0e25c129953c86cd63232a694fda81d87
Vadim Kiniabaev <thekinv21@gmail.com>
2026-09-03 16:54:15 +0300

    feat:[master][Vadim]: Spring boot application dockerize is started

---
+++ src/main/java/com/SpringBootAppDockerize/SpringBootAppDockerizeApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootAppDockerize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppDockerizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppDockerizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBootAppDockerize/SpringBootAppDockerizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootAppDockerize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppDockerizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
